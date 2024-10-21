24601f23aa55a4f212ceae005862a2ad680ec4f6
BluezConcepts <smans.elias@gmail.com>
2024-10-21 15:04:53 +0200

    added env credentials

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-// importeren  van de express module is node_modules
+// Importeren van de express module in node_modules
 const express = require('express');
+const Database = require('./classes/database.js');
 
-//aanmaken van een express app
+// Aanmaken van een express app
 const app = express();
 
-//endpoints
-app.get('/', (req, res) =>{
-    res.send("Hello World");
+// Endpoints
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get('/api/artists', (req, res) =>{
-    res.send(["JB","Beyonce"]);
+app.get('/api/artists', (req, res) => {
+    const db = new Database();
+    db.getQuery('SELECT * FROM artists').then((artists) => {
+        res.send(artists);
+    });
+});
+
+app.get('/api/songs', (req, res) => {
+    const db = new Database();
+    db.getQuery(`
+        SELECT
+            song_id, s.name AS songname, a.name AS artistname
+        FROM
+            songs AS s
+                INNER JOIN
+                    artists AS a
+                        ON
+                            s.artist_id = a.artist_id;
+    `).then((songs) => {
+        res.send(songs);
+    });
 });
 
 
-//starten van de erver en op welke port de server moet luisteren
-app.listen(3000, () =>{
-    console.log("server is running on port 3000");
+// Starten van de server en op welke port de server moet luistere
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
